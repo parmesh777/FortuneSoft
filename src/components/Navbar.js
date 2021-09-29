4b333d3bb7e2f5fb748ee3f1a6cbd4b8667930b2
parmesh777 <parmesh.kandregula@gmail.com>
2021-09-29 20:47:04 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h3>
+        Wooky <span>Movies</span>
+      </h3>
+    </nav>
+  );
+};
+
+export default Navbar;
